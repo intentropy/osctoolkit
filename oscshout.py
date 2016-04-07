8efce690ef2b8858b21952cf8aa62e1ab1d641c6
Shane Hutter <shanehutter@yahoo.com>
2016-04-07 10:10:20 +1000

    committing oscshout.py

---
+++ oscshout.py
@@ -0,0 +1,108 @@
+#!/bin/python
+'''
+OSC Shout
+  oscshout.py
+
+    Written By: Shane Hutter
+    
+    Required Dependencies:  python >= 3.5, pyliblo
+
+      This python script, and all of OSC_Tools is licensed
+      under the GNU GPL version 3
+
+      This program sends OSC Messages via the command line
+'''
+
+import liblo, sys
+
+#program CONST
+IP_PORT_PATH_ARG_INDEX=1
+IP_PORT_PATH_INDEX=0
+IP_INDEX=0
+PORT_PATH_INDEX=1
+PORT_INDEX=0
+FIRST_PATH_DIR_INDEX=1
+
+#OSC CONST
+LAST_OSC_ARG_INDEX=0
+TOTAL_NON_OSC_ARG_INDICES=2
+
+#program vars
+helpAndExit=False
+
+#osc Vars
+oscPathDir=[]
+oscPath='/'
+oscArgList=[]
+
+def sendOSC(target, path, args):
+    #send osc messages in this function
+    #send up to 8 arguments in a message
+    if len(args)==1:
+        liblo.send(target, path, args[0])
+    elif len(args)==2:
+        liblo.send(target, path, args[0], args[1])
+    elif len(args)==3:
+        liblo.send(target, path, args[0], args[1], args[2])
+    elif len(args)==4:
+        liblo.send(target, path, args[0], args[1], args[2], args[3])
+    elif len(args)==5:
+        liblo.send(target, path, args[0], args[1], args[2], args[3], args[4])
+    elif len(args)==6:
+        liblo.send(target, path, args[0], args[1], args[2], args[3], args[4], args[5])
+    elif len(args)==7:
+        liblo.send(target, path, args[0], args[1], args[2], args[3], args[4], args[5], args[6])
+    elif len(args)==8:
+        liblo.send(target, path, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7])
+    else:
+        global helpAndExit
+        helpAndExit=True
+    return
+
+#parse arguments
+if len(sys.argv)<=2:
+    helpAndExit=True
+else:
+    try:
+        #1st argument syntax: IP:Port/osc/path
+        #the following arguments are based into a list (up to 8)
+        #into sendOSC(target, '/osc/path', argumentList)
+        oscIpPortPath=sys.argv[IP_PORT_PATH_ARG_INDEX].split()[IP_PORT_PATH_INDEX]
+        oscTargetIp=oscIpPortPath.split(':')[IP_INDEX]
+        oscTargetPort=int(oscIpPortPath.split(':')[PORT_PATH_INDEX].split('/')[PORT_INDEX])
+        for pathDir in range(FIRST_PATH_DIR_INDEX,len(oscIpPortPath.split(':')[PORT_PATH_INDEX].split('/'))):
+            oscPathDir.append(oscIpPortPath.split(':')[PORT_PATH_INDEX].split('/')[pathDir])
+        for oscPathDirIndex in range(0,len(oscPathDir)):    
+            oscPath+=oscPathDir[oscPathDirIndex]+'/'
+                
+    except:
+        helpAndExit=True
+    #grab the osc message arguments and store in list
+    firstOscArgIndex=TOTAL_NON_OSC_ARG_INDICES-len(sys.argv)
+    for oscArg in range(firstOscArgIndex, LAST_OSC_ARG_INDEX):
+        oscArgList.append(sys.argv[oscArg])
+    for oscArg in range(0,len(oscArgList)):
+        try:
+            #make arg an int
+            oscArgList[oscArg]=int(oscArgList[oscArg])
+        except:
+            try:
+                #make arg a float
+                oscArgList[oscArg]=float(oscArgList[oscArg])
+            except:
+                #keep arg a string
+                oscArgList[oscArg]=str(oscArgList[oscArg])
+                
+#print help and exit
+if helpAndExit==True:
+    print('Help File')
+    sys.exit(0)
+        
+#create OSC Client
+try:
+    oscTarget=liblo.Address(oscTargetIp, oscTargetPort)
+except liblo.AddressError as err:
+    print(str(err))
+    sys.exit()
+
+sendOSC(oscTarget, oscPath, oscArgList)
