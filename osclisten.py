811b402ba2b6226ced0bf1ca0d50f52f1e2ab2d9
Shane Hutter <shanehutter@yahoo.com>
2016-04-28 09:36:51 +1000

    28/04/16 Update

--- osclisten.py
+++ osclisten.py
@@ -40,10 +40,16 @@ echoReg=[]
 CONFIG_PROPERTY_ARG=0
 CONFIG_VALUE_ARG=1
 #config
-configFileName='osctools.conf'
-configFile=open(configFileName,'r')
-configLines=configFile.read().split('\n')
-configFile.close()
+try:
+    configFileName='osctools.conf'
+    configFile=open(configFileName,'r')
+    configLines=configFile.read().split('\n')
+except:
+    configFileName='/etc/osctools.conf'
+    configFile=open(configFileName,'r')
+    configLines=configFile.read().split('\n')
+finally:
+    configFile.close()
 for lineRead in configLines:
     if lineRead!="" and lineRead.strip().startswith('#')==False:
         #verbosity settings
