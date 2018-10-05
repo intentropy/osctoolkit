e22422c80d6e720fad8cfa3108de45da8c59ff5d
Shane Hutter <shane.hutter86@gmail.com>
2018-10-05 20:29:28 +1000

    Distutils script
    A setup.py file for installing osctoolkit.  Take caution, this etup script has not been fully tested as of yet.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+    Setup script for installing OSC Toolkit
+"""
+
+from distutils.core import setup
+
+setup(
+        name                = 'osctoolkit'                          ,
+        version             = '0.0.2'                               ,
+        author              = 'Shane Hutter'                        ,
+        author_email        = 'shane@intentropycs.com'              ,
+        description         = 'Python wrapper module for xdotool'   ,
+        long_description    = open( 'README.md' ).read()            ,
+        license             = open( 'LICENSE' ).read()              ,
+        packages            = [ 'OSCToolkit' , ]                    ,
+        data_files          = [
+            ( '/usr/bin'                ,   # Executable Scripts
+                [
+                    'osclisten'     ,
+                    'oscshout'      ,
+                    'oscwhispers'   ,
+                    ]                                   ,
+            ( '/etc'                    ,   # Configuration files
+                [ 'osctoolkit.conf' , ]                 ,
+                )   ,
+            ( '/usr/share/osctoolkit'   ,   # User resources
+                [ 'example.otw' , ]                     ,
+                )   ,
+            ( '/usr/lib/systemd/user'   ,   # Systemd units/timers
+                [ 'systemd/oscwhispersd.service' , ]    ,
+                )   ,
+            ]                                                       ,
+
+        )
+
